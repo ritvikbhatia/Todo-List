e0551559888454c940282f5ddb864ee780910521
Ritvik Bhatia <ritvikbhatia99@gmail.com>
2020-06-08 00:32:10 +0530

    setup ejs template engine

--- controllers/home_controller.js
+++ controllers/home_controller.js
@@ -1,3 +1,3 @@
 module.exports.home=function(req,res){
-    res.send("yo");
+    res.render('home.ejs')
 }
--- index.js
+++ index.js
@@ -6,6 +6,8 @@ const port=8000;
 
 const app=express();
 
+app.set('view engine', 'ejs');
+
 app.use('/',require('./routes/index'))
 
 app.listen(port);
