0e15a2fca494e1b4ff0b3bea73902165ba9fbe98
Ritvik Bhatia <ritvikbhatia99@gmail.com>
2020-06-08 12:49:22 +0530

    add layouts and partials

--- index.js
+++ index.js
@@ -1,12 +1,30 @@
 //required modules
 const express=require('express');
+const sassMiddleware = require('node-sass-middleware');
+const path = require('path');
+const expressLayouts=require('express-ejs-layouts');
 
 //port defination 
 const port=8000;
 
 const app=express();
 
+app.use(sassMiddleware({
+    src: './assets/scss',
+    dest: './assets/css',
+    debug: true,
+    indentedSyntax:false,
+    outputStyle: 'extended',
+    prefix: '/css'
+}));
+
+app.use(express.static('./assets'));
 app.set('view engine', 'ejs');
+app.set('views', './views');
+app.use(expressLayouts);
+
+app.set("layout extractScripts", true)
+app.set("layout extractStyles", true)
 
 app.use('/',require('./routes/index'))
 
